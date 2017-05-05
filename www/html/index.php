31602fff42cdc645f94acb7a5c0ef4e32b4a89a1
kieranmacrae <macrae.kieran@gmail.com>
2017-05-05 10:48:25 +1000

    Initialised React.js package.json

--- www/html/index.php
+++ www/html/index.php
@@ -1,9 +1,25 @@
+<!DOCTYPE>
 <?php include "../inc/dbinfo.inc"?>
 <html>
   <head>
-    <title>Illaware Business Chamber | Placeholder</title>
+    <title>Illawara Business Chamber | Login</title>
+    <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
+    <div class="pageHeader"></div>
+    <div class="page">
+      <div class="signinBox">
+        <img class="logo" src="img/nswbc-logo.png" alt="NSW Business Chamber Logo">
+        <form class="signInForm">
+        <p class="signInLabel">Email Address</p>
+        <input type="text" class="signInInput" name="email">
+        <p class="signInLabel">Password</p>
+        <input type="password" class="signInInput" label="password">
+        <input type="submit" class="signInButton" value="Sign in">
+        <p class="forgotPassword"><a href="#">Forgot Password?</a></p>
+        </form>
+      </div>
+    </div>
     <?php echo '<p>Placeholder for Illawara Business Chamber Client Relations Management System</p>'; ?>
     <?php
       $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
