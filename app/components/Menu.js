55e3fcbae25fab1817b64a688174e0645e89962d
DESKTOP-RG9HU8O\Stef <stefaniemarkovska@hotmail.com>
2017-08-20 15:55:57 +1000

    no message

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -14,7 +14,7 @@ class Menu extends React.Component {
         <li><NavLink exact activeClassName='active-route' to='/'>
           Home
         </NavLink></li>
-        <li><NavLink activeClassName='active-route' to='php/profile.php'>
+        <li><NavLink activeClassName='active-route' to='profile'>
           My Profile
         </NavLink></li>
         <li><NavLink activeClassName='active-route' to='php/calendar.php'>
