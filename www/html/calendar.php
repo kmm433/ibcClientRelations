fe7f4330c7601ed6859243bd14c5da6debbc0e66
DESKTOP-RG9HU8O\Stef <stefaniemarkovska@hotmail.com>
2017-08-20 15:10:18 +1000

    commit title change

--- www/html/calendar.php
+++ www/html/calendar.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Illawarra Business Chamber | KBASE</title>
+    <title>Illawarra Business Chamber | Calendar</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
